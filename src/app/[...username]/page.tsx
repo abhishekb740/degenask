670a1b30460f1472ad2f801d631b5cbd0df2c979
Abhishek <abhishekb740@gmail.com>
2024-05-03 23:47:39 +0530

    Share on Warpcast Button created and copy link button created

--- src/app/[...username]/page.tsx
+++ src/app/[...username]/page.tsx
@@ -7,15 +7,18 @@ import {
 } from "@privy-io/react-auth";
 import Logo from "../components/Logo";
 import DegenLogo from "../../../public/assets/DegenLogo.png";
+import FarcasterLogo from "../../../public/assets/FarcasterLogo.png";
+import CopyLogo from "../../../public/assets/CopyLogo.png";
+import Button from "../components/Button";
 
 const Profile = ({ params: { username } }) => {
     const { ready, authenticated, user, createWallet } = usePrivy();
     console.log(user?.farcaster);
 
     return (
-        <div className="flex justify-center items-center pt-24 m-4">
-            <div className="bg-[white]  min-h-[30rem] p-8 h-full w-full sm:h-2/3 sm:w-1/3 rounded-lg">
-                <div className="flex-col gap-4">
+        <div className="flex justify-center items-center pt-20 m-4">
+            <div className="bg-[white]  min-h-[30rem] p-8 h-full w-full lg:h-2/3 lg:w-1/3 rounded-lg">
+                <div className="flex-col">
                     <div className="flex gap-6">
                         <img
                             src={user?.farcaster?.pfp!}
@@ -65,7 +68,7 @@ const Profile = ({ params: { username } }) => {
                             Set Price
                         </button>
                     </div>
-                    <div className="flex justify-between px-4">
+                    <div className="flex justify-between px-4 mb-4">
                         <div className="text-[#818898]">
                             0 DEGEN
                         </div>
@@ -76,12 +79,29 @@ const Profile = ({ params: { username } }) => {
                             </select>
                         </div>
                     </div>
-                    <div>
-                        
-                    </div>
                 </div>
-                <div>
-
+                <div className="flex mb-32">
+                    <input className="w-full rounded-md p-2" style={{ border: '1px solid black' }} placeholder="Search Users" />
+                </div>
+                <div className="flex justify-between p-2 w-full">
+                    <div >
+                        <button className={`flex bg-[#6537B1] p-1.5 text-[white] justify-evenly items-center gap-2 rounded-md`}>
+                            <Logo src={FarcasterLogo} height={20} width={20} />
+                            <div className='font-medium md:text-sm lg:text-lg xl:text-sm'>
+                                Share on Warpcast
+                            </div>
+                        </button>
+                    </div>
+                    <div >
+                        <button className={`flex bg-[white] p-1.5 text-[#545454] justify-evenly items-center gap-2 rounded-md`}>
+                            <div style={{border: '1px solid #545454'}} >
+                                <Logo src={CopyLogo} height={20} width={20} />
+                            </div>
+                            <div className='font-medium md:text-sm lg:text-lg xl:text-sm'>
+                                Copy Link
+                            </div>
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
