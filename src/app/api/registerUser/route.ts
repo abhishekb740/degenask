998d048653bda19d1f0b3399a418eadb70a7fa51
Abhishek <abhishekb740@gmail.com>
2024-05-03 09:28:39 +0530

    API Routes created

---
+++ src/app/api/registerUser/route.ts
@@ -0,0 +1,8 @@
+import { NextResponse, NextRequest } from "next/server";
+
+export async function POST(request: NextRequest) {
+    console.log(request.body);
+    const body = await request.json();
+    const {name, image, price, bio } = body;
+    return NextResponse.json({name, image, price, bio, message: "User registered successfully"});
+}
---
+++ src/app/api/setUpPrice/route.ts
@@ -0,0 +1,8 @@
+import {NextRequest, NextResponse} from "next/server";
+
+export async function PATCH (request: NextRequest) {
+    const body = await request.json();
+    const { price } = body;
+    console.log(price);
+    return NextResponse.json({price: price, message: "Price set up successfully"});
+}
