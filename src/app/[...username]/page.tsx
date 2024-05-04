e0a5d534d8907244bdcdb54cc194c09e03481e61
Abhishek <abhishekb740@gmail.com>
2024-05-04 12:45:37 +0530

    Conditional component to ask Question added

--- src/app/[...username]/page.tsx
+++ src/app/[...username]/page.tsx
@@ -60,11 +60,14 @@ const Profile = ({ params: { username } }) => {
                             </div>
                         </div>
                     </div>
+                </div>
+                {/* Conditional Rendering of this component */}
+                {/* <div className="flex-col">
                     <div className="flex justify-between mb-6">
                         <div>
                             Minimum Ask
                         </div>
-                        <button className="text-[#009DF5]" >
+                        <button className="text-[#009DF5]">
                             Set Price
                         </button>
                     </div>
@@ -79,30 +82,53 @@ const Profile = ({ params: { username } }) => {
                             </select>
                         </div>
                     </div>
-                </div>
-                <div className="flex mb-32">
-                    <input className="w-full rounded-md p-2" style={{ border: '1px solid black' }} placeholder="Search Users" />
-                </div>
-                <div className="flex justify-between p-2 w-full">
-                    <div >
-                        <button className={`flex bg-[#6537B1] p-1.5 text-[white] justify-evenly items-center gap-2 rounded-md`}>
-                            <Logo src={FarcasterLogo} height={20} width={20} />
-                            <div className='font-medium md:text-sm lg:text-lg xl:text-sm'>
-                                Share on Warpcast
-                            </div>
-                        </button>
+                    <div className="flex mb-32">
+                        <input className="w-full rounded-md p-2" style={{ border: '1px solid black' }} placeholder="Search Users" />
                     </div>
-                    <div >
-                        <button className={`flex bg-[white] p-1.5 text-[#545454] justify-evenly items-center gap-2 rounded-md`}>
-                            <div style={{border: '1px solid #545454'}} >
-                                <Logo src={CopyLogo} height={20} width={20} />
-                            </div>
-                            <div className='font-medium md:text-sm lg:text-lg xl:text-sm'>
-                                Copy Link
-                            </div>
-                        </button>
+                    <div className="flex justify-between p-2 w-full">
+                        <div>
+                            <button className={`flex bg-[#6537B1] p-1.5 text-[white] justify-evenly items-center gap-2 rounded-md`}>
+                                <Logo src={FarcasterLogo} height={20} width={20} />
+                                <div className='font-medium md:text-sm lg:text-lg xl:text-sm'>
+                                    Share on Warpcast
+                                </div>
+                            </button>
+                        </div>
+                        <div>
+                            <button className={`flex bg-[white] p-1.5 text-[#545454] justify-evenly items-center gap-2 rounded-md`}>
+                                <div style={{ border: '1px solid #545454' }} >
+                                    <Logo src={CopyLogo} height={20} width={20} />
+                                </div>
+                                <div className='font-medium md:text-sm lg:text-lg xl:text-sm'>
+                                    Copy Link
+                                </div>
+                            </button>
+                        </div>
+                    </div>
+                </div> */}
+
+
+                {/* /////////////////////////////////////  Conditional Component  //////////////////////////////////////// */}
+                <div className="flex-col">
+                    <div className="mb-6">
+                        <textarea placeholder="Ask Me Anything" style={{ border: '1px solid #818898' }} className="flex rounded-md p-2 w-full" ></textarea>
+                    </div>
+                    <div  className="mb-8">
+                        <div>
+                            Price
+                        </div>
+                        <input style={{ border: '1px solid #818898' }}  className="flex rounded-md p-2 w-full" placeholder="Min 500 $Degen" ></input>
+                    </div>
+                    <div className="">
+                        <button className="bg-[#009DF5] w-full p-2 rounded-md text-white">Submit Your Question</button>
                     </div>
                 </div>
+
+                {/* /////////////////////////////////////  Conditional Component  //////////////////////////////////////// */}
+                <div>
+
+                </div>
+
             </div>
         </div>
     )
