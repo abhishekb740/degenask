8c9bae7f04c27de0201056c6a5e51fbd9236347a
Abhishek <abhishekb740@gmail.com>
2024-05-09 22:23:36 +0530

    flamekaiser/style: Added scroll on feeds page of the profile

--- src/components/profile/feed.tsx
+++ src/components/profile/feed.tsx
@@ -4,7 +4,7 @@ import type { Profile } from "@/types";
 export default function Feed({ user }: Profile) {
   return (
     <div>
-      <div className="flex-col">
+      <div className="flex-col max-h-[20rem] overflow-y-scroll">
         <div className="bg-[#F6F8FA] p-2 rounded-md flex-col mb-4">
           <div className="mb-4">
             How and What Resources you will suggest to build on Farcaster? What are your top
@@ -15,6 +15,32 @@ export default function Feed({ user }: Profile) {
             <div>{user?.username}</div>
           </div>
           <div>Replied</div>
+        </div>
+        <div className="bg-[#F6F8FA] p-2 rounded-md flex-col mb-4">
+          <div className="mb-4">
+            How and What Resources you will suggest to build on Farcaster? What are your top
+            recommended FC users i should follow to learn more?
+          </div>
+          <div className="flex gap-2 items-center mb-4">
+            <div>asked by</div>
+            <div>{user?.username}</div>
+          </div>
+          <div>
+            <button>Answer</button>
+          </div>
+        </div>
+        <div className="bg-[#F6F8FA] p-2 rounded-md flex-col mb-4">
+          <div className="mb-4">
+            How and What Resources you will suggest to build on Farcaster? What are your top
+            recommended FC users i should follow to learn more?
+          </div>
+          <div className="flex gap-2 items-center mb-4">
+            <div>asked by</div>
+            <div>{user?.username}</div>
+          </div>
+          <div>
+            <button>Answer</button>
+          </div>
         </div>
         <div className="bg-[#F6F8FA] p-2 rounded-md flex-col mb-4">
           <div className="mb-4">
