d0cea94acd688aafcd210ae28dcac5a3c31af9bc
Abhishek <abhishekb740@gmail.com>
2024-05-11 21:16:45 +0530

    fix: Changed the Metadata URL

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import "@rainbow-me/rainbowkit/styles.css";
 import Providers from "./providers";
 import { Toaster } from "react-hot-toast";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
   title: "DegenAsk",
@@ -12,7 +13,7 @@ export const metadata: Metadata = {
   openGraph: {
     type: "website",
     locale: "en_US",
-    url: "https://degenask.me",
+    url: "https://degenask.vercel.app/",
     title: "DegenAsk",
     description:
       "Ask anything you're curious about, learn from the creator's thoughts, and earn $DEGEN for your questions.",
@@ -36,6 +37,7 @@ export default function RootLayout({
     <html lang="en">
       <body className="h-screen bg-gradient-radial from-[#80e5f7] via-[#2dd1fe] to-[#06acff]">
         <Providers>{children}</Providers>
+        <Analytics />
         <Toaster />
       </body>
     </html>
