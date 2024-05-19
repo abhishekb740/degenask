aa02c72eeb0eb7d993f2ec94982fd4882949fcbe
Abhishek <abhishekb740@gmail.com>
2024-05-19 11:11:36 +0530

    feat: seachbar added to navbar
    fix: resolve rebase conflicts

--- src/components/shared/header.tsx
+++ src/components/shared/header.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
-import { setCreator } from "@/app/_actions/queries";
+import { setCreator, getAllUsers } from "@/app/_actions/queries";
 import FarcasterIcon from "@/icons/farcaster";
 import { authAtom, authMethodAtom } from "@/store";
 import { User } from "@/types";
@@ -10,6 +10,7 @@ import { useRouter } from "next/navigation";
 import { useState } from "react";
 import toast from "react-hot-toast";
 import { MdNotificationsActive } from "react-icons/md";
+import { IoIosSearch } from "react-icons/io";
 
 export default function Header({ users }: { users: User[] }) {
   const [isDropdownOpen, setIsDropdownOpen] = useState<boolean>(false);
@@ -19,10 +20,14 @@ export default function Header({ users }: { users: User[] }) {
   const authMethod = useAtomValue(authMethodAtom);
   const setAuthMethod = useSetAtom(authMethodAtom);
   const { wallets } = useWallets();
+  const [searchQuery, setSearchQuery] = useState<string>("");
 
   const toggleDropdown = () => {
     setIsDropdownOpen((prevState) => !prevState);
   };
+
+  const filteredUsers = users.filter((user) => user.username.includes(searchQuery.toLowerCase()));
+
   const { logout } = useLogout({
     onSuccess: () => {
       setIsDropdownOpen(false);
@@ -85,15 +90,44 @@ export default function Header({ users }: { users: User[] }) {
           </Link>
         </div>
       )}
-      <div className="absolute w-full grid grid-row-[0fr_1fr_1fr] gap-y-4 sm:grid-cols-[0.8fr_2fr_1fr] md:grid-cols-[1fr_2.5fr_1fr] lg:grid-cols-[1fr_3fr_1fr] xl:grid-cols-[2.2fr_8fr_2fr] mt-14 items-center justify-center">
-        <span className="hidden sm:visible sm:flex"></span>
-        <span className="flex flex-row gap-2 items-center justify-center">
+      <div className="w-full grid grid-row-[0fr_1fr_1fr] gap-y-4 sm:grid-cols-[0.8fr_2fr_1fr] md:grid-cols-[1fr_2.5fr_1fr] lg:grid-cols-[1fr_3fr_1fr] xl:grid-cols-[2.2fr_8fr_2fr] mt-14 items-center justify-center">
+          <span className="flex flex-row gap-2 items-center justify-center">
           <img src="/degenask.png" className="w-6 h-6 object-cover" alt="logo" />
           <p className="text-[#A36EFD] md:text-xl lg:text-2xl font-title">degenask.me</p>
         </span>
-        {authenticated ? (
+        <div className="relative flex flex-col items-center">
+        <div className="flex flex-row w-1/2 rounded-[5rem] py-1 border border-neutral-300 px-5 mb-4 items-center justify-center">
+          <IoIosSearch size={30} className="text-neutral-400" />
+          <input
+            className="flex ml-4 w-full py-2 focus:outline-none"
+            placeholder="Discover Creators"
+            onChange={(e) => setSearchQuery(e.target.value)}
+          />
+        </div>
+        {searchQuery && (
+          <div className="absolute top-full flex flex-col z-10 max-h-[13rem] border border-neutral-200 bg-white/90 backdrop-blur-lg w-1/2 rounded-lg shadow-lg scroll-smooth scrollbar">
+            {filteredUsers.length ? (
+              filteredUsers.map((user) => {
+                return (
+                  <button
+                    key={user.username}
+                    className="flex flex-row gap-3 hover:bg-neutral-200 items-center w-full px-5 py-2 cursor-pointer"
+                    onClick={() => router.push(`/${user.username}`)}
+                  >
+                    <span className="w-6 h-6 bg-gradient-to-br from-emerald-100 to-teal-300 rounded-full"></span>
+                    <p className="text-lg font-primary">{user.username}</p>
+                  </button>
+                );
+              })
+            ) : (
+              <p className="ml-4 text-lg py-2 text-neutral-800 font-primary">No Creators Found</p>
+            )}
+          </div>
+        )}
+      </div>
+      {authenticated ? (
           <div
-            className="relative flex flex-row px-6 py-3 w-fit justify-center items-center font-bold gap-3 text-neutral-700 bg-white hover:cursor-pointer rounded-xl"
+            className="flex flex-row px-6 py-3 w-fit justify-center items-center font-bold gap-3 text-neutral-700 bg-white hover:cursor-pointer rounded-xl"
             onClick={toggleDropdown}
           >
             <img
